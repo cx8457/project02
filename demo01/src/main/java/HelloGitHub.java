bd0fdb5ea1ca0bbb0c472d2e7f249346427a604e
cx <845792780@qq.com>
2019-01-18 23:22:28 +0800

    Initial commit

---
+++ demo01/src/main/java/HelloGitHub.java
@@ -0,0 +1,10 @@
+/**
+ * @author Administrator
+ * @date 2019/1/18 23:20
+ * @description
+ */
+public class HelloGitHub {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
